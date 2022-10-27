790a6618c8bd4c166d2d117cd483944020dbceeb
Diego García Rodríguez del Campo <diegomysterio@gmail.com>
2022-10-26 22:52:54 -0500

    Update CHT.cpp

--- DP/CHT.cpp
+++ DP/CHT.cpp
@@ -1,55 +1,30 @@
-// CONVEX HULL TRICK STUFF
-
-struct line
-{
-    ll m, b;
-};
-
-vector<line> ch;
-ll opt;
-
-// Regresa true si l3 o l1 esconden a l2 siempre
-bool bad(ll l1, ll l2, ll l3)
-{
-    long double i1 = (long double)(ch[l1].b - ch[l3].b) / (long double)(ch[l3].m - ch[l1].m);
-    long double i2 = (long double)(ch[l1].b - ch[l2].b) / (long double)(ch[l2].m - ch[l1].m);
-    return i1 < i2;
-}
-
-// Agrega una nueva linea ( slope estrictamente menor ) a la estrtuctura
-void add(ll m, ll b)
-{
-    ch.push_back({m, b});
-    // Checa si existen lineas irrelevantes
-    while (ch.size() >= 3 && bad(ch.size() - 3, ch.size() - 2, ch.size() - 1))
-        ch.erase(ch.end() - 2);
-}
-
-// Regresa 0 si x esta en el rango de la linea ln , -1 si esta a la izq y 1 si esta a la der
-ll inRange(ll ln, ll x)
-{
-    // Checa si esta a la izq
-    // interseccion entre ln y (ln-1) tiene x = (b1-b2)/(m2-m1)
-    if (ln > 0 && (long double)x < (long double)(ch[ln].b - ch[ln - 1].b) / (long double)(ch[ln - 1].m - ch[ln].m))
-        return -1;
-    if (ln + 1 < ch.size() && (long double)(ch[ln + 1].b - ch[ln].b) / (long double)(ch[ln].m - ch[ln + 1].m) < (long double)x)
-        return 1;
-    return 0;
-}
-
-// Regresa el segmento con minimo y para una x dada con las lineas actualmente en la estructura
-ll query(ll x)
-{
-    ll l = 0, r = ch.size() - 1;
-    while (l <= r)
-    {
-        ll mid = (l + r) / 2;
-        ll donde = inRange(mid, x);
-        if (!donde)
-            return mid;
-        if (donde == -1)
-            r = mid - 1;
-        else
-            l = mid + 1;
+struct Line {
+    mutable ll k, m, p;
+    bool operator<(const Line& o) const {
+        return Q ? p < o.p : k < o.k;
     }
-}
+};
+struct LineContainer : multiset<Line> {
+    const ll inf = LLONG_MAX;
+    ll div(ll a, ll b){
+        return a / b - ((a ^ b) < 0 && a % b);
+    }
+    bool isect(iterator x, iterator y) {
+        if (y == end()) { x->p = inf; return false; }
+        if (x->k == y->k) x->p = x->m > y->m ? inf : -inf;
+        else x->p = div(y->m - x->m, x->k - y->k);
+        return x->p >= y->p;
+    }
+    void add(ll k, ll m) {
+        auto z = insert({k, m, 0}), y = z++, x = y;
+        while (isect(y, z)) z = erase(z);
+        if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
+        while ((y = x) != begin() && (--x)->p >= y->p)
+            isect(x, erase(y));
+    }
+    ll query(ll x) {
+        assert(!empty());
+        Q = 1; auto l = *lower_bound({0,0,x}); Q = 0;
+        return l.k * x + l.m;
+    }
+};
