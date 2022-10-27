a350b6a87a6b5b4c82cbd49447517b7a099f8c71
Diego García Rodríguez del Campo <diegomysterio@gmail.com>
2022-10-26 19:57:53 -0500

    Update DPOptimizations.cpp

--- DP/DPOptimizations.cpp
+++ DP/DPOptimizations.cpp
@@ -1,7 +1,15 @@
 /* 
 Divide and Conquer
   IF -> dp(i, j) = min(0<=k<=j) dp(i - 1, k - 1) + C(k + j)
-  THEN -> opt(i, j) <= opt(i, j + 1)
+  AND HOLDS -> opt(i, j) <= opt(i, j + 1)
   Complexity from O(nm^2) to O(nmlgm)
   NOTES -> Process row to row, checking mid and dividing optk
  */ 
+
+/*
+Knuths Opt
+  IF -> dp(i, j) = min(i<=k<j) dp(i, k) + dp(k + 1, j) + C(i, j)
+  AND HOLDS -> opt(i, j - 1) <= opt(i, j) <= opt(i + 1, j)
+  Complexity from O(n^3) to O(n^2)
+  NOTES -> Process row to row, checking mid and dividing optk
+*/
